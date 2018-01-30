885e9ec6fa504e9c74715dd004c544609e1650f2
Rafael Bastiani <rafacdb@gmail.com>
2018-01-30 13:49:20 -0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <h1>Rafael Bastiani</h1>
+    </header>
+    <div className="content">
+      <nav className="App-side">
+        <div className="profile-photo-container">
+          <img
+            alt=""
+            className="profile-photo"
+            src="https://s3-sa-east-1.amazonaws.com/fotos-bastiani/avatar2.jpg"
+          />
+          <br />
+          <b>Rafael Campos de Bastiani</b>
+        </div>
+        <ul className="ul-no-bullet">
+          <li>
+            <a href="https://github.com/Bastiani">
+              <i className="fa fa-github" aria-hidden="true" /> GitHub
+            </a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/in/bastiani/">
+              <i className="fa fa-linkedin" aria-hidden="true" /> Linkedin
+            </a>
+          </li>
+          <li>
+            <a href="https://www.facebook.com/rbastiani/">
+              <i className="fa fa-facebook-official" aria-hidden="true" /> Facebook
+            </a>
+          </li>
+          <li>
+            <a href="https://twitter.com/RBastiani">
+              <i className="fa fa-twitter" aria-hidden="true" /> Twitter
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <article className="App-intro">
+        Olá, sou o Rafael, desenvolvedor de software.<br />
+        Tenho 36 anos, 14 anos de experiência, moro em Caxias do Sul/RS.<br />
+        <h2>Habilidades</h2>
+        <ul className="lists">
+          <li>Delphi</li>
+          <li>JavaScript(ES6, ES7)</li>
+          <li>RubyOnRails</li>
+          <li>React</li>
+          <li>NodeJS</li>
+          <li>GraphQL</li>
+          <li>PostgreSQL</li>
+          <li>MongoDB</li>
+        </ul>
+        <h2>Palestras</h2>
+        <ul className="lists">
+          <li>
+            <a href="http://meetup-graphql.surge.sh/#/">GraphQL</a>
+            <br />
+            <a href="https://www.meetup.com/pt-BR/Rede-Neural/events/246024897/">Meetup</a>
+          </li>
+        </ul>
+        <h2>Projetos</h2>
+        <ul className="lists">
+          <li>RFDevCondomínios (RubyOnRails)</li>
+        </ul>
+      </article>
+    </div>
+  </div>
+);
+
+export default App;
---
+++ src/LayoutComponents.js
@@ -0,0 +1,70 @@
+import styled from 'styled-components';
+
+const LayoutAdmin = styled.div`
+  min-height: 550px;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  flex-flow: row;
+  @media all and (max-width: 640px) {
+    flex-direction: column;
+  }
+`;
+
+export const Article = styled.article`
+  padding: 5px;
+  background-color: rgb(255, 248, 211);
+  color: rgb(37, 37, 37);
+  flex: 3 1 60%;
+  order: 2;
+  @media all and (max-width: 640px) {
+    order: 0;
+  }
+`;
+
+export const Nav = styled.nav`
+  border-right: 16px ${props => props.theme.bahLayoutNavBoderRightColor} solid;
+  background: ${props => props.theme.bahLayoutNavBackgroundColor};
+  order: 1;
+  ${props =>
+    (props.bahHidden
+      ? `flex: 0 6 0;
+        overflow: hidden;
+        &:hover {
+          flex-basis: 10em;
+        }
+      `
+      : `flex: 0 6 ${props.theme.bahNavDefaultFlexBasis};`)};
+  transition: flex-basis 500ms ease-in-out;
+  @media all and (max-width: 640px) {
+    order: 0;
+    border-top: 16px #342 solid;
+    border-right: 0px;
+  }
+`;
+
+export const Aside = styled.aside`
+  padding: 5px;
+  background: ${props => props.theme.bahLayoutAsideBackgroundColor};
+  flex: 1 6 20%;
+  order: 3;
+  @media all and (max-width: 640px) {
+    order: 0;
+    min-height: 50px;
+    max-height: 50px;
+  }
+`;
+
+export const Header = styled.div`
+  display: block;
+  padding: 5px;
+  min-height: 40px;
+  background-color: rgb(0, 0, 0);
+  color: rgb(255, 104, 35);
+`;
+
+export const Footer = Header.extend`
+  background-color: black;
+`;
+
+export default LayoutAdmin;
